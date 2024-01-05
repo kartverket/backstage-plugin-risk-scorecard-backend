e0ae59f208617aa7cc35c439d870b37eb23307d0
fosso <bragefosso@hotmail.com>
2024-01-05 16:19:02 +0100

    Implemented a simple decrypt util function that currently reads from a saved file

--- src/main/kotlin/no/kvros/KvRosApplication.kt
+++ src/main/kotlin/no/kvros/KvRosApplication.kt
@@ -2,10 +2,16 @@ package no.kvros
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
+import no.kvros.utils.decrypt.decryptYamlData
 
 @SpringBootApplication
 class KvRosApplication
 
 fun main(args: Array<String>) {
     runApplication<KvRosApplication>(*args)
+    val encData = File(".sikkerhet/ros/kryptert.ros.yaml").readText()
+    println("encrypted Data: " + encData)
+    val decData = decryptYamlData(encData)
+    println("decrypted data: " + decData)
 }
