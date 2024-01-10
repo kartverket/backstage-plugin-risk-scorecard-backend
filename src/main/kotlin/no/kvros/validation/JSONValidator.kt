a3152e7560a64d201e6c5914065b200cff81ec25
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-01-10 14:34:59 +0100

    Add error print

--- src/main/kotlin/no/kvros/validation/JSONValidator.kt
+++ src/main/kotlin/no/kvros/validation/JSONValidator.kt
@@ -3,5 +3,11 @@ package no.kvros.validation
 import net.pwall.json.schema.JSONSchema
 
 object JSONValidator {
-    fun validateJSON(decryptedJson: String): Boolean = JSONSchema.parseFile(".sikkerhet/ros_schema_no_v1_0.json").validate(decryptedJson)
+    fun validateJSON(decryptedJson: String): Boolean {
+        val output = JSONSchema.parseFile(".sikkerhet/ros_schema_no_v1_0.json").validateBasic(decryptedJson)
+        output.errors?.forEach {
+            println("${it.error} - ${it.instanceLocation}")
+        }
+        return output.valid
+    }
 }
