4bf40bf0b6f4dcac635185d489030843f9b44bfe
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-01-10 11:09:32 +0100

    Refactor decryption function (#12)

--- src/main/kotlin/no/kvros/encryption/SopsEncryptorForYaml.kt
+++ src/main/kotlin/no/kvros/encryption/SopsEncryptorForYaml.kt
@@ -3,40 +3,28 @@ package no.kvros.encryption
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-
 object SopsEncryptorForYaml {
     private val processBuilder =
         ProcessBuilder("sops", "--input-type", "yaml", "--output-type", "json", "--decrypt", "/dev/stdin")
+            .redirectErrorStream(true)
 
     private const val EXECUTION_STATUS_OK = 0
 
-    fun decrypt(ciphertext: String): String? {
-        processBuilder.redirectErrorStream(true)
-
+    fun decrypt(ciphertext: String): String? =
         try {
-            val process = processBuilder.start()
+            processBuilder.start().run {
+                outputStream.buffered().also { it.write(ciphertext.toByteArray()) }.close()
+                when (waitFor()) {
+                    EXECUTION_STATUS_OK -> BufferedReader(InputStreamReader(inputStream)).readText()
 
-            // Write the encrypted data to the process's input stream
-            process.outputStream.buffered().use { it.write(ciphertext.toByteArray()) }
-            process.outputStream.close()
-            // Wait for the process to complete
-            process.waitFor()
-
-            if (process.exitValue() != EXECUTION_STATUS_OK) {
-                println("IOException from decrypting yaml with error code: ${process.exitValue()}")
-                return null
+                    else -> {
+                        System.err.println("IOException from decrypting yaml with error code: ${exitValue()}")
+                        null
+                    }
+                }
             }
-
-            // Read the decrypted data from the process's output stream
-            val reader = BufferedReader(InputStreamReader(process.inputStream))
-            val decryptedData = reader.readText()
-
-            return decryptedData
         } catch (e: Exception) {
             e.printStackTrace()
+            null
         }
-
-        return null
-    }
-
 }
