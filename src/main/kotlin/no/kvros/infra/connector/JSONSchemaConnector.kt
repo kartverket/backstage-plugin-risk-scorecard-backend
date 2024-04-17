7ce7f04317579b4de29f54a9b6a299c463a662bd
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-04-17 16:06:40 +0200

    bug: only create access token for the repository needed (#101)
    * Only create access token for the repository needed
    
    * Fix json schema bug

--- src/main/kotlin/no/kvros/infra/connector/JSONSchemaConnector.kt
+++ src/main/kotlin/no/kvros/infra/connector/JSONSchemaConnector.kt
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Component
 class JSONSchemaConnector(
     @Value("\${json-schema-base-url}") baseUrl: String,
 ) : WebClientConnector(baseUrl) {
-
     fun fetchJSONSchema(schemaVersion: String): String? {
         return try {
             webClient.get()
-                .uri("/ros_schema_no_v3_2.json")
+                .uri("/ros_schema_no_v$schemaVersion.json")
                 .header("Accept", "application/vnd.github.json")
                 .retrieve()
                 .bodyToMono(String::class.java)
