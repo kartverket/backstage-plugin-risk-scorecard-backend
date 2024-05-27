43eb994a2aa63eb5a31bead83863087b08d1b50a
larsore <larssorensen98@hotmail.com>
2024-05-27 18:01:03 +0200

    Update

--- src/main/kotlin/no/risc/infra/connector/models/AccessTokens.kt
+++ src/main/kotlin/no/risc/infra/connector/models/AccessTokens.kt
@@ -5,9 +5,7 @@ import no.risc.github.GithubAccessToken
 data class AccessTokens(
     val githubAccessToken: GithubAccessToken,
     val gcpAccessToken: GCPAccessToken,
-) {
-    fun isValid(): Boolean = gcpAccessToken.value.isNotBlank() && githubAccessToken.value.isNotBlank()
-}
+)
 
 data class GCPAccessToken(val value: String)
 
