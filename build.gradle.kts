a72cf0e4b255562f9e9a9bd545ecf52950994129
Torbjørn Tessem <torbjorn.tessem@kartverket.no>
2024-07-31 14:16:38 +0200

    same version

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
