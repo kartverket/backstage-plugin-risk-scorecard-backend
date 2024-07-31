f2873b082607d7477a4eb635810841bfa803ac77
Frikk <frikk.hald.andersen@bekk.no>
2024-07-31 14:21:19 +0200

    remove old encrypt code

--- src/main/kotlin/no/risc/encryption/SOPS.kt
+++ src/main/kotlin/no/risc/encryption/SOPS.kt
@@ -1,8 +1,6 @@
 package no.risc.encryption
 
-import no.risc.exception.exceptions.SopsEncryptionException
 import no.risc.infra.connector.models.GCPAccessToken
-import no.risc.infra.connector.models.sensor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory.getLogger
 import java.io.BufferedReader
@@ -14,35 +12,22 @@ object SOPS : ISopsEncryption {
     private val logger: Logger = getLogger(SOPS::class.java)
 
     private val sopsCmd = listOf("sops")
-    private val encrypt = listOf("encrypt")
     private val decrypt = listOf("decrypt")
     private val inputTypeYaml = listOf("--input-type", "yaml")
-    private val inputTypeJson = listOf("--input-type", "json")
-    private val outputTypeYaml = listOf("--output-type", "yaml")
     private val outputTypeJson = listOf("--output-type", "json")
-    private val encryptConfig = listOf("--encrypt-config")
     private val inputFile = listOf("/dev/stdin")
 
     private val processBuilder = ProcessBuilder().redirectErrorStream(true)
     private const val EXECUTION_STATUS_OK = 0
-
-    private fun toEncryptionCommand(
-        config: String,
-        accessToken: String,
-    ): List<String> =
-        sopsCmd + encrypt + inputTypeJson + outputTypeYaml + encryptConfig + config + inputFile +
-            gcpAccessToken(
-                accessToken,
-            )
 
     private fun toDecryptionCommand(
         accessToken: String,
         sopsPrivateKey: String,
     ): List<String> =
         sopsCmd + ageSecret(sopsPrivateKey) + decrypt + inputTypeYaml + outputTypeJson + inputFile +
-            gcpAccessToken(
-                accessToken,
-            )
+                gcpAccessToken(
+                    accessToken,
+                )
 
     private fun gcpAccessToken(accessToken: String): List<String> = listOf("--gcp-access-token", accessToken)
 
@@ -70,34 +55,7 @@ object SOPS : ISopsEncryption {
             }
     }
 
-    override fun encrypt(
-        text: String,
-        config: String,
-        gcpAccessToken: GCPAccessToken,
-        riScId: String,
-    ): String {
-        return try {
-            processBuilder
-                .command(toEncryptionCommand(config, gcpAccessToken.value))
-                .start()
-                .run {
-                    outputStream.buffered().also { it.write(text.toByteArray()) }.close()
-                    val result = BufferedReader(InputStreamReader(inputStream)).readText()
-                    when (waitFor()) {
-                        EXECUTION_STATUS_OK -> result
-                        else -> throw SopsEncryptionException(
-                            message = "Failed when encrypting RiSc with ID: $riScId by running sops command: ${
-                                toEncryptionCommand(
-                                    config,
-                                    gcpAccessToken.sensor().value,
-                                )
-                            } with error message: $result",
-                            riScId = riScId,
-                        )
-                    }
-                }
-        } catch (e: Exception) {
-            throw e
-        }
+    override fun encrypt(text: String, config: String, gcpAccessToken: GCPAccessToken, riScId: String): String {
+        TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/no/risc/risc/RiScService.kt
+++ src/main/kotlin/no/risc/risc/RiScService.kt
@@ -97,7 +97,6 @@ class RiScService(
     @Value("\${sops.ageKey}") val ageKey: String,
     @Value("\${filename.prefix}") val filenamePrefix: String,
     private val cryptoService: CryptoServiceIntegration,
-    @Value("\${featureToggle.useCryptoServiceForEncryption}") val useCryptoServiceForEncryption: Boolean,
     @Value("\${featureToggle.useCryptoServiceForDecrypt}") val useCryptoServiceForDecrypt: Boolean,
 ) {
     private val logger = LoggerFactory.getLogger(RiScService::class.java)
@@ -172,6 +171,7 @@ class RiScService(
                                                 }
                                                 it
                                             }
+
                                             RiScStatus.SentForApproval -> it
                                             RiScStatus.Published -> it
                                         }
@@ -301,7 +301,7 @@ class RiScService(
         if (jsonSchema.status != GithubStatus.Success) {
             throw JSONSchemaFetchException(
                 message =
-                    "Failed when fetching JSON schema from Github with status: ${jsonSchema.status}, " +
+                "Failed when fetching JSON schema from Github with status: ${jsonSchema.status}, " +
                         "and error message: ${jsonSchema.data}",
                 riScId = riScId,
             )
@@ -329,11 +329,7 @@ class RiScService(
         val config = removePathRegex(sopsConfig.data())
 
         val encryptedData: String =
-            if (useCryptoServiceForEncryption) {
-                cryptoService.encryptPost(content.riSc, config, accessTokens.gcpAccessToken, riScId)
-            } else {
-                SOPS.encrypt(content.riSc, config, accessTokens.gcpAccessToken, riScId)
-            }
+            cryptoService.encryptPost(content.riSc, config, accessTokens.gcpAccessToken, riScId)
 
         try {
             val hasClosedPR =
