cd5fd8da7cd0c04ab716d034d87002ab75754996
Tollefsen <havard.tollefsen@gmail.com>
2024-08-30 08:18:43 +0200

    Add comments and better names to difference functions

--- src/main/kotlin/no/risc/utils/DifferenceComparer.kt
+++ src/main/kotlin/no/risc/utils/DifferenceComparer.kt
@@ -48,6 +48,7 @@ object FlatMapRiScUtil {
      * @return A List<String> with each key in a RiSc and its value.
      */
     fun flatten(map: Map<String, Any?>): List<String> {
+        // Gives us a string in the format of "{key}: {value}"
         fun returnString(
             key: String,
             value: String,
@@ -57,35 +58,40 @@ object FlatMapRiScUtil {
 
         return map.flatMap { entry ->
 
-            if (entry == null) {
-                listOf("${entry.key}")
-            }
-
             when (val value = entry.value) {
                 is List<*> -> {
+                    // When we encounter a List<*> we know that the value comes from a parsed JsonElement and is therefore a JsonArray.
                     val list = jsonParseToElement(value.toString()).jsonArray
 
+                    // We should show empty lists
                     if (list.size == 0) {
                         listOf(returnString(entry.key, value.toString()))
                     } else {
+                        // Convert the list to a Map<String, Any?> and recursively call flatten
                         val newMap = value.indices.associate { "${entry.key}/$it" to list[it] }
                         flatten(newMap)
                     }
                 }
 
                 is JsonElement -> {
+                    // When we encounter a JsonElement, we want to try and parse it.
                     val json = jsonParseToElement(value.toString())
                     when (json) {
                         is JsonObject -> {
+                            // then, if the element is another JsonObject we want to flatten its content.
                             if (json.keys.isEmpty()) {
+                                // Unless it is empty, there is no content to flatten.
                                 listOf(returnString(entry.key, value.toString()))
                             } else {
+                                // Runs flatten recursively on JsonObjects content.
                                 json.flatMap { flatten(mapOf("${entry.key}/${it.key}" to it.value)) }
                             }
                         }
+                        // If the element is a normal value, stop the recursion.
                         else -> listOf(returnString(entry.key, value.toString()))
                     }
                 }
+                // Failsafe for unhandled value types.
                 else -> listOf("${entry.key} - $value is Unknown")
             }
         }
@@ -97,6 +103,10 @@ object FlatMapRiScUtil {
     }
 }
 
+private fun List<String>.toDifferenceMap(): Map<String, Any> {
+    return this.associate { it.split(": ").first() to it.split(": ").last() }
+}
+
 class DifferenceException(message: String) : Exception(message)
 
 @Throws(DifferenceException::class)
@@ -106,14 +116,18 @@ fun diff(
 ): Difference {
     val json = Json { ignoreUnknownKeys = true }
     try {
+        // Parse JsonObjects from riscs and transfrom to maps.
         val baseJsonObject = json.parseToJsonElement(base).jsonObject.toMap()
         val headJsonObject = json.parseToJsonElement(head).jsonObject.toMap()
 
-        val result1 = FlatMapRiScUtil.flatten(baseJsonObject).associate { it.split(": ").first() to it.split(": ").last() }
-        val result2 = FlatMapRiScUtil.flatten(headJsonObject).associate { it.split(": ").first() to it.split(": ").last() }
+        // Flatten out the structures and transform to maps.
+        val result1 = FlatMapRiScUtil.flatten(baseJsonObject).toDifferenceMap()
+        val result2 = FlatMapRiScUtil.flatten(headJsonObject).toDifferenceMap()
 
+        // Calculate the difference.
         val difference = Maps.difference(result1, result2)
 
+        // Transform the differences to string lists.
         val entriesDiffering: List<String> = difference.entriesDiffering().entries.map { it.key + ": " + it.value }
         val entriesOnLeft = difference.entriesOnlyOnLeft().entries.map { it.key + ": " + it.value }
         val entriesOnRight = difference.entriesOnlyOnRight().entries.map { it.key + ": " + it.value }
--- src/test/kotlin/no/risc/DifferenceComparerTests.kt
+++ src/test/kotlin/no/risc/DifferenceComparerTests.kt
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test
 
 class DifferenceComparerTests {
     @Suppress("ktlint:standard:max-line-length")
-    private var baseContent = "{\"schemaVersion\":\"4.0\", \"myObject\":{}, \"myList\": [], \"title\":\"Versjon 4 \",\"scope\":\"Finne ut av siste versjon\",\"valuations\":[],\"scenarios\":[{\"title\":\"Rekkefølge\",\"scenario\":{\"ID\":\"h4i9Y\",\"description\":\"hei\",\"threatActors\":[],\"vulnerabilities\":[],\"risk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000},\"actions\":[],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}},{\"title\":\"Testing 4.0\",\"scenario\":{\"ID\":\"rFww8\",\"description\":\"sfd\",\"threatActors\":[\"Organised crime\"],\"vulnerabilities\":[\"Flawed design\",\"Unauthorized access\",\"Unmonitored use\",\"Excessive use\"],\"risk\":{\"summary\":\"\",\"probability\":1,\"consequence\":1000},\"actions\":[{\"title\":\"\",\"action\":{\"ID\":\"27A7C\",\"description\":\"Går dette?\",\"status\":\"Not started\",\"url\":\"www.nrk.no\"}}],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}}]}"
+    private var baseContent = "{\"schemaVersion\":\"3.8\", \"title\":\"Versjon 4 \",\"scope\":\"Finne ut av siste versjon\",\"valuations\":[],\"scenarios\":[{\"title\":\"Rekkefølge\",\"scenario\":{\"ID\":\"h4i9Y\",\"description\":\"hei\",\"threatActors\":[],\"vulnerabilities\":[],\"risk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000},\"actions\":[],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}},{\"title\":\"Testing 4.0\",\"scenario\":{\"ID\":\"rFww8\",\"description\":\"sfd\",\"threatActors\":[\"Organised crime\"],\"vulnerabilities\":[\"Flawed design\",\"Unauthorized access\",\"Unmonitored use\",\"Excessive use\"],\"risk\":{\"summary\":\"\",\"probability\":1,\"consequence\":1000},\"actions\":[{\"title\":\"\",\"action\":{\"ID\":\"27A7C\",\"description\":\"Går dette?\",\"status\":\"Not started\",\"url\":\"www.nrk.no\"}}],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}}]}"
 
     @Suppress("ktlint:standard:max-line-length")
-    private var headContent = "{\"schemaVersion\":\"3.8\",\"title\":\"Versjon 4 \",\"scope\":\"Finne ut av siste versjon\",\"valuations\":[],\"scenarios\":[{\"title\":\"Rekkefølge\",\"scenario\":{\"ID\":\"h4i9Y\",\"description\":\"\",\"vulnerabilities\":[\"Skummel type\"],\"risk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000},\"actions\":[],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}},{\"title\":\"Testing 4.0\",\"scenario\":{\"ID\":\"rFww8\",\"description\":\"sfd\",\"threatActors\":[\"Organised crime\"],\"vulnerabilities\":[\"Flawed design\",\"Unauthorized access\",\"Unmonitored use\",\"Excessive use\"],\"risk\":{\"summary\":\"\",\"probability\":1,\"consequence\":1000},\"actions\":[{\"title\":\"\",\"action\":{\"ID\":\"27A7C\",\"description\":\"Går dette?\",\"status\":\"Not started\",\"url\":\"www.nrk.no\"}}],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}}]}"
+    private var headContent = "{\"schemaVersion\":\"4.0\",\"title\":\"Versjon 4 \",\"valuations\":[],\"scenarios\":[{\"title\":\"Rekkefølge\",\"scenario\":{\"ID\":\"h4i9Y\",\"description\":\"\",\"vulnerabilities\":[\"Skummel type\"],\"risk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000},\"actions\":[],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}},{\"title\":\"Testing 4.0\",\"scenario\":{\"ID\":\"rFww8\",\"description\":\"sfd\",\"threatActors\":[\"Organised crime\"],\"vulnerabilities\":[\"Flawed design\",\"Unauthorized access\",\"Unmonitored use\",\"Excessive use\"],\"risk\":{\"summary\":\"\",\"probability\":1,\"consequence\":1000},\"actions\":[{\"title\":\"\",\"action\":{\"ID\":\"27A7C\",\"description\":\"Går dette?\",\"status\":\"Not started\",\"url\":\"www.nrk.no\"}}],\"remainingRisk\":{\"summary\":\"\",\"probability\":0.01,\"consequence\":1000}}}]}"
 
     @Test
     fun `when two normal riscs with minor differences is checked then it shows deletions, additions, and changes `() {
@@ -19,7 +19,7 @@ class DifferenceComparerTests {
         val result = diff(baseContent, headContent)
 
         // Assert
-        assertEquals(4, result.entriesOnLeft.size)
+        assertEquals(3, result.entriesOnLeft.size)
         assertEquals(2, result.difference.size)
         assertEquals(1, result.entriesOnRight.size)
     }
@@ -29,7 +29,7 @@ class DifferenceComparerTests {
         // Arrange
         val corruptContent = "[\"not actually a JsonObject\"]"
 
-        // Act
+        // Act & Assert
         assertThrowsExactly(DifferenceException::class.java) { diff(baseContent, corruptContent) }
     }
 }
