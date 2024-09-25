9af3c9fa32780d931f1e21e9a3cca0a55127c2e5
fosso <bragefosso@hotmail.com>
2024-09-25 16:44:50 +0200

    filter out the commit by timestamp instead of removing n commits. should work better with different merging strategies

--- src/main/kotlin/no/risc/github/GithubConnector.kt
+++ src/main/kotlin/no/risc/github/GithubConnector.kt
@@ -310,6 +310,7 @@ class GithubConnector(
 
                 // Latest commit timestamp on default branch that includes changes on this riSc
                 val latestCommitTimestamp = fetchLatestCommitTimestampOnDefault(owner, repository, accessToken, riScId)
+                println(latestCommitTimestamp)
 
                 // Check if previous commits on draft branch ahead of main requires approval
                 val commitMessages =
@@ -320,7 +321,7 @@ class GithubConnector(
                             accessToken,
                             riScId,
                             it,
-                        ).map { it.commit }.dropLast(2)
+                        ).map { it.commit }.filterNot {it.committer.date == latestCommitTimestamp}
                     }
 
                 val commitsAheadOfMainRequiresApproval =
