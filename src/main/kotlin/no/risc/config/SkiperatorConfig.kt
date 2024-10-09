7f74c919be0faeec8ea052b3e6b72ac95899cda0
Lars Sørensen <larssorensen98@hotmail.com>
2024-10-09 16:49:03 +0200

    WIP

--- src/main/kotlin/no/risc/config/SkiperatorConfig.kt
+++ src/main/kotlin/no/risc/config/SkiperatorConfig.kt
@@ -4,6 +4,7 @@ import io.kubernetes.client.openapi.apis.CustomObjectsApi
 import io.kubernetes.client.util.Config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import kotlin.properties.Delegates
 
 @Configuration
 @ConfigurationProperties(prefix = "skiperator")
