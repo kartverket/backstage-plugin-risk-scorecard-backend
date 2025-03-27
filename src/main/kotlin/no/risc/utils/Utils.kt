0099256254a723957794bc50951290921a8ba4e2
Halvard Hummel <halvard.hummel@bekk.no>
2025-03-27 15:23:49 +0100

    Stop using the depricated method randomAlphanumeric from apache-commons-lang. Removes the library as a dependency, as this is the only use of the library.

--- src/main/kotlin/no/risc/utils/Utils.kt
+++ src/main/kotlin/no/risc/utils/Utils.kt
@@ -1,7 +1,8 @@
 package no.risc.utils
 
-import org.apache.commons.lang3.RandomStringUtils
 import java.util.Base64
+import java.util.stream.Stream
+import kotlin.random.Random
 
 data class Repository(
     val repositoryOwner: String,
@@ -12,6 +13,24 @@ fun String.encodeBase64(): String = Base64.getEncoder().encodeToString(toByteArr
 
 fun String.decodeBase64(): String = Base64.getMimeDecoder().decode(toByteArray()).decodeToString()
 
-fun generateRiScId(filenamePrefix: String) = "$filenamePrefix-${RandomStringUtils.randomAlphanumeric(5)}"
+fun generateRiScId(filenamePrefix: String) = "$filenamePrefix-${generateRandomAlphanumericString(5)}"
 
-fun generateSopsId() = "sops-${RandomStringUtils.randomAlphanumeric(5)}"
+fun generateSopsId() = "sops-${generateRandomAlphanumericString(5)}"
+
+private val alphaNumericChars: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+/**
+ * Generates a random alphanumeric string
+ *
+ * @param length The number of characters in the generated string
+ * @param random The random number generator to use, defaults to `Random.Default` if not supplied.
+ */
+fun generateRandomAlphanumericString(
+    length: Int,
+    random: Random = Random,
+): String =
+    Stream
+        .generate { alphaNumericChars.random(random) }
+        .limit(length.toLong())
+        .toArray()
+        .joinToString(separator = "")
