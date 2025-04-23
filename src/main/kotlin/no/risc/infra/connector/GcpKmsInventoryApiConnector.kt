1da997772237c8b7a949e2a5b1c9c20cb7162a1e
halvardhummel <halvard.hummel@bekk.no>
2025-04-23 08:14:54 +0200

    Remove unused code related to Google services (#402)
    * Remove old, unused code in the Google Service.
    
    * Fix typing warning on LOGGER
    
    * Remove duplicate log statement (in GoogleControllerIntegration and directely the same log statement in GoogleServiceIntegration.getGcpCryptoKeys).

--- src/main/kotlin/no/risc/infra/connector/GcpKmsInventoryApiConnector.kt
+++
@@ -1,8 +0,0 @@
-package no.risc.infra.connector
-
-import org.springframework.stereotype.Component
-
-@Component
-class GcpKmsInventoryApiConnector(
-    baseUrl: String = "https://kmsinventory.googleapis.com",
-) : WebClientConnector(baseUrl)
