d44963609b1903f747b615679b41675361dce447
Halvard Hummel <halvard.hummel@bekk.no>
2025-05-28 08:36:40 +0200

    Add change tracking for migrations from 3.3 to 4.0 and 4.0 to 4.1.

---
+++ src/main/kotlin/no/risc/utils/comparison/MigrationDTOs.kt
@@ -0,0 +1,62 @@
+package no.risc.utils.comparison
+
+import kotlinx.serialization.Serializable
+
+data class MigrationChanges(
+    val migrationChange40: MigrationChange40? = null,
+    val migrationChange41: MigrationChange41? = null,
+)
+
+// Changes for the migration from version 3.3 to 4.0
+
+@Serializable
+data class MigrationChange40(
+    val scenarios: List<MigrationChange40Scenario>,
+)
+
+@Serializable
+data class MigrationChange40Scenario(
+    val title: String,
+    val id: String,
+    val removedExistingActions: String? = null,
+    val changedVulnerabilities: List<MigrationChangedValue<String>>,
+    val changedActions: List<MigrationChange40Action>,
+)
+
+@Serializable
+data class MigrationChange40Action(
+    val title: String,
+    val id: String,
+    val removedOwner: String? = null,
+    val removedDeadline: String? = null,
+)
+
+// Changes for the migration from version 4.0 to 4.1
+
+@Serializable
+data class MigrationChange41(
+    val scenarios: List<MigrationChange41Scenario>,
+)
+
+@Serializable
+data class MigrationChange41Scenario(
+    val title: String,
+    val id: String,
+    var changedRiskProbability: MigrationChangedValue<Double>? = null,
+    var changedRiskConsequence: MigrationChangedValue<Int>? = null,
+    var changedRemainingRiskProbability: MigrationChangedValue<Double>? = null,
+    var changedRemainingRiskConsequence: MigrationChangedValue<Int>? = null,
+) {
+    fun hasChanges() =
+        changedRiskConsequence !== null ||
+            changedRiskProbability !== null ||
+            changedRemainingRiskConsequence != null ||
+            changedRemainingRiskProbability != null
+}
+
+// General change object
+@Serializable
+data class MigrationChangedValue<T>(
+    val oldValue: T,
+    val newValue: T,
+)
