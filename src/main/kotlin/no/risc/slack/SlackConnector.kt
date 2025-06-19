4dc60d2053eaa9647b22272c34157871d85552cc
annaaurora <anna-aurora@live.no>
2025-06-19 10:44:56 +0200

    endret sendFeedback til en suspend-funksjon og byttet block() med awaitBody, for å unngå å blokkere tråden mens vi venter på svar fra Slack

--- src/main/kotlin/no/risc/slack/SlackConnector.kt
+++ src/main/kotlin/no/risc/slack/SlackConnector.kt
@@ -3,19 +3,19 @@ package no.risc.slack
 import no.risc.infra.connector.WebClientConnector
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.awaitBody
 
 @Component
 class SlackConnector(
     @Value("\${slack.feedback.webhook.url}")
     private val webhookUrl: String,
 ) : WebClientConnector(webhookUrl) {
-    fun sendFeedBack(message: String) {
+    suspend fun sendFeedBack(message: String) {
         webClient
             .post()
             .bodyValue(SlackMessageDTO(message))
             .retrieve()
-            .bodyToMono(String::class.java)
-            .block()
+            .awaitBody<String>()
     }
 }
 
