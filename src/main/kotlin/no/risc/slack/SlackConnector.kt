03874ac96abbf176705b1f370ee1966b19135751
annaaurora <anna-aurora@live.no>
2025-06-20 12:38:44 +0200

    lagt til dokumentasjon for SlackConnector

--- src/main/kotlin/no/risc/slack/SlackConnector.kt
+++ src/main/kotlin/no/risc/slack/SlackConnector.kt
@@ -6,6 +6,15 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.awaitBody
 
+/**
+ * Connector for sending feedback messages to a Slack webhook.
+ *
+ * Uses a configured Slack webhook URL to send messages in JSON format using `SlackMessageDTO`.
+ * The message is posted as a simple HTTP POST request using WebClient.
+ *
+ * @param webhookUrl The Slack webhook URL configured in application properties.
+ */
+
 @Component
 class SlackConnector(
     @Value("\${slack.feedback.webhook.url}")
