a00d1cf363a91f52e8730a03118c2969fed3384a
annaaurora <80164059+annaaurora@users.noreply.github.com>
2026-05-13 08:41:17 +0200

    fix: Improve access token validation failed exception logging (#704)
    * add cause to exception and chain it in validationService
    
    * fix: replace ex.response with inline DTO
      construction
    
    * ktlint

--- src/main/kotlin/no/risc/exception/exceptions/AccessTokenValidationFailedException.kt
+++ src/main/kotlin/no/risc/exception/exceptions/AccessTokenValidationFailedException.kt
@@ -1,14 +1,6 @@
 package no.risc.exception.exceptions
 
-import no.risc.risc.models.ProcessRiScResultDTO
-import no.risc.risc.models.ProcessingStatus
-
-data class AccessTokenValidationFailedException(
-    val response: Any =
-        ProcessRiScResultDTO(
-            riScId = "",
-            status = ProcessingStatus.AccessTokensValidationFailure,
-            statusMessage = "An error occurred when validating access token.",
-        ),
+class AccessTokenValidationFailedException(
     override val message: String,
-) : Exception()
+    cause: Throwable? = null,
+) : Exception(message, cause)
